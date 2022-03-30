6b455326b4d0a2562ce85593ad2ef7619a006f8f
Julia <julyaremko@gmail.com>
2022-03-30 11:21:21 +0300

    creation Auth components

--- components/Auth/Login/LoginContent/LoginContent.vue
+++ components/Auth/Login/LoginContent/LoginContent.vue
@@ -4,7 +4,7 @@
     <div class="form-group">
       <label for="exampleInputEmail1">Email address</label>
       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-             placeholder="Enter email">
+             placeholder="Enter email address">
       <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
     <div class="form-group">
